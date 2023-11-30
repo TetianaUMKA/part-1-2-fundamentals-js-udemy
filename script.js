ae7f70eda6debc32b392d4b7183b632258accfce
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-11-30 22:05:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ let person = "jonas";
 let PI = 3.1415;
 
 let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+/* The 7 primitive data types
+1. Number         let age = 23;               used for decimals and integers
+2. String         let first Name = 'Jonas';   sequence of characters
+3. Boolean        let fullAge = true;         logical type (false and true)
+4. Undefined      let children;    the value taken by the variable that is not defind('emty value')
+// 5. Null: Also means 'emty value', but not the same
+5. Symbol(ES2015): value that is unique and cannot be changed (Not useful for now)
+6. Biglnt(ES2020): Larger integers than the Number type can hold
+*/
