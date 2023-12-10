5a7d1a22f4f03c4195d3a292ce91769716c34754
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-10 22:53:00 +0200

    Update coding-challenge-3.js

--- coding-challenge-3.js
+++ coding-challenge-3.js
@@ -1,10 +1,38 @@
-let scoreDolphins = (96 + 108 + 89) / 3;
-let scoreKoalas = (88 + 91 + 110) / 3;
+// Challenge-3
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
 
-if (scoreDolphins > scoreKoalas) {
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy");
+// } else {
+//   console.log("Both win the trophy");
+// }
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log("Dolphins win the trophy");
+// } else if (scoreDolphins < scoreKoalas) {
+//   console.log("Koalas win the trophy");
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log("Both win the trophy");
+// }
+
+// Bonus Challenge-3
+const scoreDolphins = (89 + 108 + 89) / 3; // 95.33333333333333
+const scoreKoalas = (107 + 91 + 110) / 3; // 102.66666666666667
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
   console.log("Dolphins win the trophy");
-} else if (scoreDolphins < scoreKoalas) {
+} else if (scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
   console.log("Koalas win the trophy");
-} else {
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
   console.log("Both win the trophy");
+} else {
+  console.log("No one wins the trophy");
 }
