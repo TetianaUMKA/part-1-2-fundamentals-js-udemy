d461fb32ed6f944dfc9c4c72a9b6193b1ba3bee2
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-12-27 21:38:55 +0200

    Update coding-challenge-9.js

--- coding-challenge-9.js
+++ coding-challenge-9.js
@@ -1,22 +1,4 @@
-function printForecast(arr) {
-  let concatenatedString = '';
-  for (let i = 0; i < arr.length; i++) {
-    let daylyString;
-    if (typeof arr[i] !== 'number') continue;
-    if (i === 0) {
-      daylyString = `... ${arr[i]}ºC in ${i + 1} days`;
-      concatenatedString += daylyString;
-      continue;
-    }
-    daylyString = ` ... ${arr[i]}ºC in ${i + 1} days`;
-    concatenatedString += daylyString;
-    console.log(daylyString);
-  }
-  console.log(concatenatedString);
-}
-
-printForecast([17, 21, 23]); //  ... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days
-printForecast([12, 5, -5, 0, 'errors', 4]); //  ... 12ºC in 1 days ... 5ºC in 2 days ... -5ºC in 3 days ... 0ºC in 4 days ... 4ºC in 5 days
+// a easy version of the challenge
 
 function printForecast2(arr) {
   let concatenatedString = '';
@@ -28,3 +10,28 @@ function printForecast2(arr) {
 
 printForecast2([17, 21, 23]); //  ... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days
 printForecast2([12, 5, -5, 0, 4]); //  ... 12ºC in 1 days ... 5ºC in 2 days ... -5ºC in 3 days ... 0ºC in 4 days ... 4ºC in 5 days
+
+// a difficult version of the challenge
+
+function printForecast(arr) {
+  let concatenatedString = '';
+  for (let i = 0, n = 0; i < arr.length; i++, n++) {
+    let daylyString;
+    if (typeof arr[i] !== 'number') {
+      n -= 1;
+      continue;
+    }
+    if (i === 0) {
+      daylyString = `... ${arr[i]}ºC in ${n + 1} days`;
+      concatenatedString += daylyString;
+      continue;
+    }
+    daylyString = ` ... ${arr[i]}ºC in ${n + 1} days`;
+    concatenatedString += daylyString;
+    console.log(daylyString);
+  }
+  console.log(concatenatedString);
+}
+
+printForecast([17, 21, 23]); //  ... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days
+printForecast([12, 5, -5, 0, 'errors', 4]); //  ... 12ºC in 1 days ... 5ºC in 2 days ... -5ºC in 3 days ... 0ºC in 4 days ... 4ºC in 5 days
