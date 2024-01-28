e0afae8df1d15b01fff3acfd0e8467861c6011d1
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-01-28 20:58:51 +0200

    Update data-structures_modern-operators.js

--- data-structures_modern-operators.js
+++ data-structures_modern-operators.js
@@ -507,6 +507,26 @@ console.log(new Set('jonasschmedtmann').size); // 11
 
 // Map // is iterable object holds key-value pairs and remembers the original insertion order of the keys.
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+// It is bad readable, but very clever
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
   [1, 'C'],
