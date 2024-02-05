35daba318b2421eb199cf38f0aab26d66a71f26e
Tetiana Tkachuk <113261486+TetianaUMKA@users.noreply.github.com>
2024-02-05 15:03:53 +0200

    Update closer-look-at-functions.js

--- closer-look-at-functions.js
+++ closer-look-at-functions.js
@@ -53,3 +53,21 @@ newPassport(jonas5);
 console.log(jonas5.passport);
 
 checkIn(flight, jonas5);
+
+// Functions accepting Callback functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
